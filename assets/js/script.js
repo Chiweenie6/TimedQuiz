a92e7750db9a9d5c1a4c86c31fee363b76d44403
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-11-30 18:22:07 -0700

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+// Connects to the HTML
+var quizQuestions = document.getElementById("questions");
+var answerButton = document.getElementById("answer");
+
+// Creates the quiz
+function generateQuiz{
+
+}
+
+// Calls the quiz to run
+generateQuiz();
+
+var jsQuestions = [
+    {
+    question: "Which of the following are data types?"
+    answers: {
+    1: "String",
+    2: "Number",
+    3: "Boolean",
+    4: "All of the above"
+    },
+    correctAnswer: "4"
+},
+{
+    question: "Variables can be all numbers."
+    answers: {
+    1: "True.",
+    2: "False.",
+    },
+    correctAnswer: "2"
+},
+{
+    question: "Which of the following are comparatives?"
+    answers: {
+    1: "||",
+    2: "&",
+    3: "&|",
+    4: "|&"
+    },
+    correctAnswer: "1"
+},
+{
+    question: "How do you find the length of ?"
+    answers: {
+    1: "String",
+    2: "Number",
+    3: "Boolean",
+    4: "All of the above"
+    },
+    correctAnswer: "4"
+},
+{
+    question: "Which of the following are data types?"
+    answers: {
+    1: "String",
+    2: "Number",
+    3: "Boolean",
+    4: "All of the above"
+    },
+    correctAnswer: "4"
+},
+{
+    question: "Which of the following are data types?"
+    answers: {
+    1: "String",
+    2: "Number",
+    3: "Boolean",
+    4: "All of the above"
+    },
+    correctAnswer: "4"
+},
+];
---
+++ assets/js/script1.js
@@ -0,0 +1,13 @@
+// Connects to the HTML
+var getResults = document.getElementById("score");
+
+
+// Shows results of the quiz
+function highScore {
+
+}
+
+
+// 
+getResults.addEventListener("click", highScore);
+getResults.onclick = function {alert ("Next");};
