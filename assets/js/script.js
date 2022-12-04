4ce990f32d7ed9d1e1debfb0c2d05eac7534ee8d
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-04 13:45:49 -0700

    getting closer on the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
-// Connects to the HTML
 
-// var beginBtn = document.querySelector(".beginBtn button");
-// var ruleBox = document.querySelector("#ruleBox");
-// var stopBtn = ruleBox.querySelector(".buttons .stop");
-// var btnNext = ruleBox.querySelector(".buttons .continue");
-// var quizContainer = document.querySelector(".quizContainer");
-// var scoreboard = document.querySelector(".scoreboard");
-// var options = document.querySelector(".options");
-// var clock = document.querySelector("header .clock");
-// var timeInfo = document.querySelector(".timer .timeRemaining");
-// var countDown = document.querySelector(".timer .tseconds");
 
 
 function beginQuiz () {
@@ -21,114 +10,135 @@ function beginQuiz () {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var quizBox = document.getElementById("questions");
-// var answerButton = document.getElementById("answer");
-// var scoreboard = document.getElementById("score");
-
-
-// makeQuiz(questions, quizBox, scoreboard, answerButton);
-
-
-// // Creates the quiz
-// function makeQuiz(questions, quizBox, scoreboard, answerButton) {
-    
-// // Shows the questions and answers
-//     function questionsAppear(questions, quizBox) {
-//         var info =[];
-//         var answers;
-
-//         for(var i = 0; i < questions.length; i++) {
-//             answers = [];
-
-//             for(number in questions[i].answers) {
-//                 answers.push(
-//                     "<label>" + "<input type='radio' name='question"+i+"' value='"+ number +"'>" + number + ": " + questions[i].answers[number] + "</label>"
-//                 );
-//             }
-
-//             info.push(
-//                 "<section class ='question'>" + questions[i].question + "</section>" + "<section class ='answers'>" + answers.join("") + "</section>"
-//             );
-//         }
-
-//         quizBox.innerHTML = info.join("");
-
-//     }
-
-//     questionsAppear(questions, quizBox);
-
-// // Shows the user's answers
-//     function finalScore(questions, quizBox, scoreboard) {
-//         var userAnswers = quizBox.querySelectorAll("answers");
-
-//         var userChoice = "";
-//         var correctAmount = 0;
-
-//         for(var i = 0; i < questions.length; i++) {
-//             userChoice = (userAnswers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-
-//             if(userChoice === questions[i].correctAnswer) {
-//                 correctAmount++;
-
-//                 userAnswers[i].style.color = "green";
-//             } else {
-//                 userAnswers[i].style.color = "orange";
-//             }
-//         }
-
-//         scoreboard.innerHTML = correctAmount + " of " + questions.length;
-
-//     }
-
-
-//     questionsAppear(questions, quizBox);
-
-//     answerButton.onclick = function() {
-//         finalScore(questions, quizBox, scoreboard);
-//     }
-
-// }
-
-
-
-
-
-// makeQuiz(questions, quizBox, scoreboard, answerButton);
+function quizBox() {
+    document.querySelector.
+
+}
+
+
+
+
+// Quiz questions
+
+let countdown = document.getElementById("quizCounter");
+let scoreboard = document.getElementById("rank");
+let clock = 60;
+let quizQuest = 0;
+let result = 0;
+
+let quizSentence = document.getElementById("quizSentence"),
+    answer1 = document.getElementById("answer1"),
+    answer2 = document.getElementById("answer2"),
+    answer3 = document.getElementById("answer3"),
+    answer4 = document.getElementById("answer4");
+
+let quizQuestions = [
+    { question: "Which of the following are data types?",
+    answers: {
+    choice1: "String",
+    choice2: "Number",
+    choice3: "Boolean",
+    choice4: "All of the above"
+    },
+    correctAnswer: "4"
+    },
+    { question: "Variables defined with 'let' ",
+    answers: {
+    choice1: "can be redeclared unlimited times.",
+    choice2: "can only be redeclared once.",
+    choice3: "cannot be redeclared.",
+    choice4: "can only be redeclared twice."
+    },
+    correctAnswer: "3"
+    },
+    { question: "Which of the following are comparatives?",
+    answers: {
+    choice1: "||",
+    choice2: "&",
+    choice3: "&|",
+    choice4: "|&"
+    },
+    correctAnswer: "1"
+    },
+    { question: "What does NaN stand for?",
+    answers: {
+    choice1: "Not a Numerator",
+    choice2: "Not a Number",
+    choice3: "Nothing is a Number",
+    choice4: "Non of the above"
+    },
+    correctAnswer: "2"
+   },
+   { question: "Which of the following is not a coding language?",
+    answers: {
+    choice1: "CSS",
+    choice2: "HTML",
+    choice3: "SOS",
+    choice4: "JS"
+    },
+    correctAnswer: "3"
+   },
+   { question: "Which of the following is a method used for an array?",
+    answers: {
+    choice1: ".pop()",
+    choice2: ".poke()",
+    choice3: ".prod()",
+    choice4: ".pour()"
+    },
+    correctAnswer: "1"
+}
+];
+
+finalCountdown();
+
+function finalCountdown () {
+    let timeFrame = setInterval( () => {
+        if(clock <= 60 && clock > 35) {
+            countdown.style.color = "green";
+        } else if (clock <= 35 && clock > 10) {
+            countdown.style.color = "orange";
+        } else if (clock <= 10 && clock >= 0) {
+            countdown.style.color = "red";
+        }
+
+        countdown.innerText = clock;
+        clock -= 1;
+
+        if (clock < 0) {
+            clearInterval(timeFrame);
+            alert("You have run out of time, please try again.")
+            location.href = "./index.html";
+        }
+    }, 1000);
+}
+
+starterGun();
+
+function starterGun () {
+    let knowledge = quizQuestions[quizQuest];
+    quizSentence.innerText = knowledge.question;
+    answer1.textContent = knowledge.choice1;
+    answer2.textContent = knowledge.choice2;
+    answer3.textContent = knowledge.choice3;
+    answer4.textContent = knowledge.choice4;
+}
+
+function theTruth(answer) {
+    if (answer === quizQuestions[quizQuest].correctAnswer) {
+        quizQuest ++;
+        result ++;
+
+        if (quizQuest < quizQuestions.length) {
+            starterGun();
+        }
+    } else {
+        clock = parseInt(countdown.innerText);
+        clock -= 10;
+        countdown.innerText = clock;
+    }
+
+    if (result === 6) {
+        let userScore = prompt("High Fvie ✋: You scored a [' + clock + '] Write your initials below to keep score.")
+        location.href = "./assets/js/script1.js";
+    }
+}
--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,61 +1,9 @@
-// Quiz questions
-var quizQuestions = [
-    {
-    question: "Which of the following are data types?",
-    answers: {
-    1: "String",
-    2: "Number",
-    3: "Boolean",
-    4: "All of the above"
-    },
-    correctAnswer: '4'
-},
-{
-    question: "Variables can be all numbers.",
-    answers: {
-    1: "True.",
-    2: "False.",
-    },
-    correctAnswer: "2"
-},
-{
-    question: "Which of the following are comparatives?",
-    answers: {
-    1: "||",
-    2: "&",
-    3: "&|",
-    4: "|&"
-    },
-    correctAnswer: "1"
-},
-{
-    question: "What does NaN stand for?",
-    answers: {
-    1: "Not a Numerator",
-    2: "Not a Number",
-    3: "Nothing is a Number",
-    4: "Non of the above"
-    },
-    correctAnswer: "2"
-},
-{
-    question: "Which of the following is not a coding language?",
-    answers: {
-    1: "CSS",
-    2: "HTML",
-    3: "SOS",
-    4: "JS"
-    },
-    correctAnswer: "3"
-},
-{
-    question: "Which of the following is a method used for an array?",
-    answers: {
-    1: ".pop()",
-    2: ".poke()",
-    3: ".prod()",
-    4: ".pour()"
-    },
-    correctAnswer: "1"
-},
-];
+
+
+
+
+
+
+
+
+
