419f6931000922d236214cfee4b703db96e032f7
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2022-12-03 21:56:37 -0700

    changed up HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,9 @@ function makeQuiz(questions, quizBox, scoreboard, answerButton) {
         for(var i = 0; i < questions.length; i++) {
             answers = [];
 
-            for(letter in questions[i].answers) {
+            for(number in questions[i].answers) {
                 answers.push(
-                    "<label>" + "<input type='radio' name='question"+i+"' value='"+ letter +"'>" + letter + ": " + questions[i].answers[letter] + "</label>"
+                    "<label>" + "<input type='radio' name='question"+i+"' value='"+ number +"'>" + number + ": " + questions[i].answers[number] + "</label>"
                 );
             }
 
@@ -37,7 +37,7 @@ function makeQuiz(questions, quizBox, scoreboard, answerButton) {
 
 // Shows the user's answers
     function finalScore(questions, quizBox, scoreboard) {
-        var userAnswers = quizBox.querySelectorAll(".answers");
+        var userAnswers = quizBox.querySelectorAll("answers");
 
         var userChoice = "";
         var correctAmount = 0;
@@ -69,64 +69,64 @@ function makeQuiz(questions, quizBox, scoreboard, answerButton) {
 
 
 // Quiz questions
-var questions = [
+var jsquestions = [
     {
     question: "Which of the following are data types?",
     answers: {
-    a: 'String',
-    b: 'Number',
-    c: 'Boolean',
-    d: 'All of the above'
+    1: "String",
+    2: "Number",
+    3: "Boolean",
+    4: "All of the above"
     },
-    correctAnswer: 'd'
-}, 
+    correctAnswer: '4'
+},
 {
     question: "Variables can be all numbers.",
     answers: {
-    a: "True.",
-    b: "False.",
+    1: "True.",
+    2: "False.",
     },
-    correctAnswer: "b"
-}, 
+    correctAnswer: "2"
+},
 {
     question: "Which of the following are comparatives?",
     answers: {
-    a: "||",
-    b: "&",
-    c: "&|",
-    d: "|&"
+    1: "||",
+    2: "&",
+    3: "&|",
+    4: "|&"
     },
-    correctAnswer: "a"
-}, 
+    correctAnswer: "1"
+},
 {
     question: "What does NaN stand for?",
     answers: {
-    a: "Not a Numerator",
-    b: "Not a Number",
-    c: "Nothing is a Number",
-    d: "Non of the above"
+    1: "Not a Numerator",
+    2: "Not a Number",
+    3: "Nothing is a Number",
+    4: "Non of the above"
     },
-    correctAnswer: "b"
-}, 
+    correctAnswer: "2"
+},
 {
     question: "Which of the following is not a coding language?",
     answers: {
-    a: "CSS",
-    b: "HTML",
-    c: "SOS",
-    d: "JS"
+    1: "CSS",
+    2: "HTML",
+    3: "SOS",
+    4: "JS"
     },
-    correctAnswer: "c"
-}, 
+    correctAnswer: "3"
+},
 {
     question: "Which of the following is a method used for an array?",
     answers: {
-    a: ".pop()",
-    b: ".poke()",
-    c: ".prod()",
-    d: ".pour()"
+    1: ".pop()",
+    2: ".poke()",
+    3: ".prod()",
+    4: ".pour()"
     },
-    correctAnswer: "a"
+    correctAnswer: "1"
 },
 ];
 
